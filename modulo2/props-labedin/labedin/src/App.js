86c2b6e28c4d86d7f868f2da592250e8a06c0596
anasammi <a.sammi1103@gmail.com>
2022-01-11 17:29:57 -0300

    Primeira parte (card pequeno ainda não estilizado)

---
+++ modulo2/props-labedin/labedin/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import CardPequeno from './components/CardPequeno/CardPequeno';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import FotoPerfil from './img/FotoPerfil.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem= {FotoPerfil} 
+          nome="Ana Sue" 
+          descricao="Oi, eu sou a Ana Sue. Antiga Médica Veterinária, futura programadora e sempre curiosa."
+        />
+
+<div className="page-section-container">
+        <h2>Contato</h2>
+        <CardPequeno 
+          imagem="https://i.pinimg.com/474x/cf/00/e9/cf00e9dbd04b0619b8a3b0a8bf9f7051.jpg" 
+          descricao="Endereço: Rua do Limoeiro, 666" 
+        />
+        
+        <CardPequeno
+          imagem="https://img2.gratispng.com/20180501/spe/kisspng-computer-icons-email-symbol-mobile-phones-share-outline-5ae89c0216cc09.9181412515251937300934.jpg" 
+          descricao="E-mail: ana@sue.com" 
+        />
+      </div>
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        />
+        
+        <CardGrande 
+          imagem="https://i0.wp.com/wp.ufpel.edu.br/medvet/files/2020/06/teste-de-logo-mv.jpg?fit=505%2C519&ssl=1" 
+          nome="Laboratório de Parasitologia" 
+          descricao="Pesquisa muda o mundo" 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
