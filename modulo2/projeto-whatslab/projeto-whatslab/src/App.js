c6edebf43625c807760cdfe6811802d1d7d1375d
anasammi <a.sammi1103@gmail.com>
2022-01-14 16:58:48 -0300

    primeiro commit

---
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          WhatsLab
+        </p>       
+      </header>
+
+      <div>
+        <p>
+          Área de mensagem
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
