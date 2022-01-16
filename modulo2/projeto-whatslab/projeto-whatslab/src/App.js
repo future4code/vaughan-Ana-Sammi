a64b77b13ef70fcbc19090f822105ab4464d0117
anasammi <a.sammi1103@gmail.com>
2022-01-16 16:27:56 -0300

    estilizadinho

--- modulo2/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,12 +1,75 @@
 import React from 'react';
 import styled from 'styled-components';
+import Baloes from './components/Baloes/Baloes';
 
 const ContainerPrincipal = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 1px solid coral;
+  height: 90vh;
+  width: 100vw;
+`
+const Titulo = styled.h1`
+  font-size: 1.5rem;
+  background-color: #DB7093;
+  width: 520px;
+  text-align: center;
+  
+`
+const AreaMensagem = styled.div`
+  width: 500px;
+  height: 200px;
+  background: linear-gradient(#FFC0CB, #FFE4E1);
+  display: flex;
+  flex-direction: column ;
+  height: 90%;
+  padding: 10px;
+`
+const AreaInput = styled.div`
+  display: flex;
+  justify-content:flex-start;
+  width: 500px;
+  margin-top: 1%;
+
+`
+const AreaInputNome = styled.input`
+  width: 20%; 
+  outline-color: #DB7093;
+  border-radius: 5px;
+  height: 1.1rem;
+  padding-left: 5px;
+  
+`
+const AreaInputMsg = styled.input`
+  width: 70%;
+  outline-color: #DB7093;
+  border-radius: 5px;
+  padding-left: 5px;
+  margin: 0 5px;
+`
+
+const Botao = styled.button`
+  border-radius: 5px;
+  background-color: #DB7093;
+  color: white;
+`
+
+const Mensagem = styled.div`
+  background-color: #FFF0F5;
+  align-self: flex-start; 
+  margin-bottom: 5px; 
+  border-radius: 5px;
+  padding: 0 5px;
+  font-weight: 450;
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.2);
+  display: flex;
+  line-height: 1.5em;
+`
+const Nome = styled.div`
+  font-weight: bold; 
+  margin-right: 5px;
+  
 `
 
 
@@ -26,7 +89,7 @@ class App extends React.Component {
    
   enviarMensagem = () => {
     const novaMensagem = {
-      nomeUsuario: `${this.state.valorInputNome}:`,
+      nomeUsuario: `${this.state.valorInputNome}: `,
       mensagemUsuario: this.state.valorInputMensagem
     };
 
@@ -47,38 +110,40 @@ onChangeInputMensagem = (event) => {
 
 
 render() {
+
   const mensagemPronta = this.state.mensagens.map((msg) => {
     return (
-      <div>
-        <p>
-        {msg.nomeUsuario} {msg.mensagemUsuario}
-        </p>
-      </div>
+      
+      <Mensagem>
+        <Nome>{msg.nomeUsuario } </Nome>
+        <div>{msg.mensagemUsuario}</div>
+      </Mensagem>
+      
     )
   });
 
 return (
   <>
   <ContainerPrincipal>
-    <div>
+    <Titulo>
       WhatsLab
-    </div>
-    <div>
+    </Titulo>
+    <AreaMensagem>
       {mensagemPronta}
-    </div>
-    <div>
-      <input
+    </AreaMensagem>
+    <AreaInput>
+      <AreaInputNome
         value={this.state.valorInputNome}
         onChange={this.onChangeInputNome}
-        placeholder="Nome de usuário"
+        placeholder="Nome"
       />
-      <input
+      <AreaInputMsg
         value={this.state.valorInputMensagem}
         onChange={this.onChangeInputMensagem}
         placeholder="Mensagem"
       />
-      <button onClick={this.enviarMensagem}>Enviar</button>
-    </div>
+      <Botao onClick={this.enviarMensagem}>Enviar</Botao>
+    </AreaInput>
   </ContainerPrincipal>
   </>
 )
