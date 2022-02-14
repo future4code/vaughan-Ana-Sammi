89582b958c2ef5d8f2fc1a03d8ea2a96b110deae
anasammi <a.sammi1103@gmail.com>
2022-02-14 15:42:50 -0300

    Projeto LabeX

---
+++ modulo3/projeto-labex/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import axios from 'axios'
+
+function App() {
+
+  //* trocar telas
+
+  return (
+    <div className="App">
+
+      //* passar por props para os filhos
+      
+     oi
+    </div>
+  );
+}
+
+export default App;
