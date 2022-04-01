5bb90ec31a403d087f4cb06c0e94199f8044f112
Ana <a.sammi1103@gmail.com>
2022-04-01 20:40:04 -0300

    mais algumas funcionalidades feitas

--- modulo4/Projeto-sistema-bancario/src/index.ts
+++ modulo4/Projeto-sistema-bancario/src/index.ts
@@ -19,9 +19,10 @@ app.post("/users", (req, res) => {
             throw new Error("Verifique se todos os campos estão preenchidos")
         }
 
+        
         let currentYear = new Date().getTime()
-        const [day, month, year] = birth.split("/")
-        const birthTime = month+"/"+day+"/"+year
+        let [day, month, year] = birth.split("/")
+        let birthTime: string = year+"-"+month+"-"+day
         let age = Date.parse(birthTime)
        
         if (currentYear - age < 18*31536000000){
@@ -74,6 +75,79 @@ app.get("/users/balance/:name/:cpf", (req, res) => {
         }
     }
     catch (e:any) {
+        res.status(errorCode).send(e.message)
+    }
+})
+
+
+app.put("/users/bankentry", (req, res) => {
+    let errorCode = 400
+    let {name, cpf, value} = req.body
+    try {
+        if (!name || !cpf || !value) {
+            errorCode = 401
+            throw new Error("Todos os campos devem ser preenchidos")
+        }
+        let userData = users.find(user => user.name.toLowerCase() === name.toLowerCase() && user.cpf === cpf)
+        if (userData === undefined) {
+            errorCode = 422
+            throw new Error("Verifique se os dados foram preenchidos corretamente")
+        } else {
+        let newBalance: number = userData.balance + value
+            userData = {
+                ...userData,
+                balance: newBalance
+            }
+        }
+        res.status(201).send("Saldo adicionado com sucesso!")
+    }
+    catch(e:any) {
+        res.status(errorCode).send(e.message)
+    }
+})
+
+
+app.put("/users/pay", (req, res) => {
+    let errorCode = 400
+    try {
+
+        let {cpf, value, date, description} = req.body
+        if(!value || !description) {
+            errorCode = 401
+            throw new Error("Preencha todos os campos")
+        }
+        // if(!date) {
+        //     let bill = {
+        //         value,
+        //         description
+        //     }
+        // }
+        
+        if(date) {
+            let paymentDate: string = date.split("/").reverse().join("-")
+            if (Date.parse(paymentDate) + 86340000 < Date.now()) {
+                errorCode =  422
+                throw new Error("A data de pagamento não pode ser anterior à data atual")
+            } else {
+                let user = users.find((user) => user.cpf === cpf)
+                console.log(user)
+                if (user === undefined) {
+                    throw new Error("Usuário não encontrado. Verifique o CPF.")
+                }
+                else if(user !== undefined) {
+                    user.transactions.push(
+                        {
+                        value,
+                        date,
+                        description
+                     })
+                    user.balance -= value
+                    res.status(201).send("Pagamento efetuado com sucesso!")
+                }
+            }
+        }
+    }
+    catch (e:any){
         res.status(errorCode).send(e.message)
     }
 })
